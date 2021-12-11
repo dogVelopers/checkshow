9ddd1c70ce8d73bbb4eec0fb4a7355020a40c68b
MojitoBar <wnehdtjr5@gmail.com>
2021-12-12 02:25:20 +0900

    feat: project init

---
+++ CheckShow/CheckShowApp.swift
@@ -0,0 +1,18 @@
+//
+//  CheckShowApp.swift
+//  CheckShow
+//
+//  Created by judongseok on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct CheckShowApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
