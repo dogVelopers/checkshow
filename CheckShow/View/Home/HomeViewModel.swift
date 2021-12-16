4529f3d2c0c922f814598c26462dec954ba3421c
wnehdtjr5@gmail.com <wnehdtjr5@naver.com>
2021-12-16 22:58:32 +0900

    add scrollview & navbar items

---
+++ CheckShow/View/Home/HomeViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewModel.swift
+//  CheckShow
+//
+//  Created by judongseok on 2021/12/16.
+//
+
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+    private let category = ["전체", "뮤지컬", "클래식", "오페라", "무용", "국악", "복합", "아동", "오픈런"]
+
+    func getCategoryCount() -> Int {
+        return category.count
+    }
+
+    func getCategory() -> [String] {
+        return category
+    }
+
+    func getCategory(n: Int) -> String {
+        return category[n]
+    }
+}
