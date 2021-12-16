235382b719795ef216c9938e63258b7520e5ccf1
MojitoBar <wnehdtjr5@gmail.com>
2021-12-17 02:17:55 +0900

    feat: navbar click action

--- CheckShow/View/Home/HomeViewModel.swift
+++ CheckShow/View/Home/HomeViewModel.swift
@@ -9,6 +9,15 @@ import SwiftUI
 
 class HomeViewModel: ObservableObject {
     private let category = ["전체", "뮤지컬", "클래식", "오페라", "무용", "국악", "복합", "아동", "오픈런"]
+    @Published private var clickedCategory = 0
+
+    func getClickedCategory() -> Int {
+        return clickedCategory
+    }
+
+    func setClickedCategory(n: Int) {
+        clickedCategory = n
+    }
 
     func getCategoryCount() -> Int {
         return category.count
